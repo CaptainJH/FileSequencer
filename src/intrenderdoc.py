f4cfc65bcb99bad3888439969a6808010c883f14
Heqi Ju <heqi.ju@autodesk.com>
2017-03-02 16:38:30 +0800

    update renderdoc integration script

--- src/intrenderdoc.py
+++ src/intrenderdoc.py
@@ -18,17 +18,12 @@ jfrogPath = "C:\\Users\\juhe\\Downloads\\jfrog.exe" # replace with your local pa
 curlPath = "C:\\Users\\juhe\\AppData\\Local\\Apps\\cURL\\bin\\curl.exe" # replace with your local path
 
 
-def FilterRETest1(p):
-    if(os.path.isdir(p)):
-        return True
-    return not FileSequencerLib.reMatch(p, "^.*vector.*$")
-
-def MakeJamfileForRenderdoc(src, filelist, dst):
+def MakeJamfileForRenderdoc(src, filelist, folderlist, dst):
     #SHA = FileSequencerLib.ArtifactorySHADict[src]
     SHA = "2a922324cc3f846a33f0d7ed701b8b7483746089"
     return FileSequencerLib.MakeJamfiles(src, filelist, dst, REPOTOP, SHA, "renderdoc", VERSION)
 
-def MakeJamfileForRenderdocCommon(src, filelist, dst):
+def MakeJamfileForRenderdocCommon(src, filelist, folderlist, dst):
     SHA = "fd2e84f4c93764ae7b8e5d879eba668d59925bb2"
     return FileSequencerLib.MakeJamfiles(src, filelist, dst, REPOTOP, SHA, "renderdoc", VERSION)
 
